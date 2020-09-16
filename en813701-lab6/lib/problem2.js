a6e78914e03f96ef09d68948eae8f80f7e3dcd4d
booknaklub <tanadol.mo@kkumail.com>
2020-09-16 17:09:44 +0700

    profile update4

--- en813701-lab6/lib/problem2.js
+++ en813701-lab6/lib/problem2.js
@@ -2,12 +2,18 @@ function readInpute(){
     outer :while(true){
         let inputNum = prompt("Enter an integer to quit");
         let allNum = [];
+        let minNum;
+        let maxNum;
+        let averageNum;
+        let totalNum = 0;
         if (inputNum >= 0 && inputNum % 1 == 0 ){
             allNum.push(inputNum);
+            totalNum +=inputNum;
         } else if (inputNum<0) {
             allNum.sort
-            let minNum = allNum[0];
-            let maxNum = allNum[];
+            minNum = allNum[0];
+            let maxNum = allNum[allNum-1];
+            let averageNum = totalNum/
         }
     }
     
