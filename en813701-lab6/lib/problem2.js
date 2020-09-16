edc76f5b997ccff6375f90a349e738862ab2017b
booknaklub <tanadol.mo@kkumail.com>
2020-09-16 16:59:47 +0700

    profile update3

---
+++ en813701-lab6/lib/problem2.js
@@ -0,0 +1,14 @@
+function readInpute(){
+    outer :while(true){
+        let inputNum = prompt("Enter an integer to quit");
+        let allNum = [];
+        if (inputNum >= 0 && inputNum % 1 == 0 ){
+            allNum.push(inputNum);
+        } else if (inputNum<0) {
+            allNum.sort
+            let minNum = allNum[0];
+            let maxNum = allNum[];
+        }
+    }
+    
+}
