1473ad00f6ad177b14536b1ff92300ee12dadca2
booknaklub <68809032+booknaklub@users.noreply.github.com>
2020-09-29 20:27:09 +0700

    updateLab6

--- en813701-lab6/lib/problem2.js
+++ en813701-lab6/lib/problem2.js
@@ -1,20 +1,43 @@
-function readInpute(){
-    outer :while(true){
-        let inputNum = prompt("Enter an integer to quit");
-        let allNum = [];
-        let minNum;
-        let maxNum;
-        let averageNum;
-        let totalNum = 0;
-        if (inputNum >= 0 && inputNum % 1 == 0 ){
-            allNum.push(inputNum);
-            totalNum +=inputNum;
-        } else if (inputNum<0) {
-            allNum.sort
-            minNum = allNum[0];
-            let maxNum = allNum[allNum-1];
-            let averageNum = totalNum/
+function readInput() {
+    let allNum = [];
+
+    while (true) {
+        let input = Number(prompt("Enter an integer (a negative integer to quit):"));
+
+        if (!isNaN(input) && input >= 0 && Number.isInteger(input)) {
+            allNum.push(input);
         }
+        if (input < 0) {
+            return allNum;
+        }
+
     }
-    
 }
+
+function minNum(allNum) {
+    let min = allNum.sort(function (x, y) {return x - y})[0]
+    return min;
+}
+
+function maxNum(allNum) {
+    let max = allNum.sort((x, y) => x - y)[allNum.length - 1]
+    return max;
+}
+
+function avgNum(allNum) {
+    let total = 0;
+    for (let number of allNum) {
+        total += Number(number);
+    }
+    return Number((total / allNum.length).toFixed(2));
+}
+
+
+
+let allNum = readInput();
+let min = minNum(allNum);
+let max = maxNum(allNum);
+let avg = avgNum(allNum);
+
+alert(`For the list ${allNum} the average is ${avg}, the minimum is ${min}, and the maximum is ${max}`);
+console.log(allNum, min, max, avg);
